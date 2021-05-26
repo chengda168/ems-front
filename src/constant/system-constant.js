71af4b3939e3eac018a772fa1961c9bdf2e62aec
oushuo <874572150@qq.com>
2021-05-26 14:56:49 +0800

    【新增】 添加系统常量

---
+++ src/constant/system-constant.js
@@ -0,0 +1,32 @@
+/** 用户类型 */
+const userType = {
+    USER_TYPE_0: "0",
+    // 园区用户
+    USER_TYPE_1: "1",
+    // 运维团队用户
+    USER_TYPE_2: "2"
+};
+
+/** 角色类型 */
+const roleType = {
+    // 平台管理用户
+    ROLE_TYPE_0: "0",
+    // 园区用户
+    ROLE_TYPE_1: "1",
+    // 运维团队用户
+    ROLE_TYPE_2: "2"
+};
+
+/** 状态 */
+const status = {
+    // 不可用
+    STATUS_0: "0",
+    // 可用
+    STATUS_1: "1"
+}
+
+export default {
+    userType,
+    status,
+    roleType
+}
